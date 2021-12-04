cc441f3ab43048c562f4c0954b33824b96bc45ae
David Surkov <surkovdavid@gmail.com>
2021-12-04 20:55:32 +0100

    04/12 2055 change

--- scripts.js
+++ scripts.js
@@ -141,3 +141,12 @@ const myArray = [[2, 3, 4], ["bull", "big"]];
 //Access Array Data with Indexes
 const myArray = [50, 60, 70];
 var myData = myArray[0]
+
+//Access Multi-Dimensional Arrays With Indexes
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  const myData = myArray[2][1];
