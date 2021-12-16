e757aea689826bd6730bfb832c2b805cf4663fdf
David Surkov <surkovdavid@gmail.com>
2021-12-16 21:05:39 +0100

    last of 16/12

--- scripts.js
+++ scripts.js
@@ -681,3 +681,224 @@ switch(val) {
 }
 
 sequentialSizes(1);
+
+/* Replacing If Else Chains with Switch
+If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements. The following: */
+function chainToSwitch(val) {
+  let answer = "";
+  if (val === "bob") {
+    answer = "Marley";
+  } else if (val === 42) {
+    answer = "The Answer";
+  } else if (val === 1) {
+    answer = "There is no #1";
+  } else if (val === 99) {
+    answer = "Missed me by this much!";
+  } else if (val === 7) {
+    answer = "Ate Nine";
+  }
+  return answer;
+}
+
+chainToSwitch(7);
+
+function chainToSwitch(val) {
+  let answer = "";
+  switch(val) {
+  case "bob":
+    answer = "Marley";
+    break;
+  case 42:
+    answer = "The Answer";
+    break;
+  case 1:
+    answer = "There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
+    break;
+  case 7:
+    answer = "Ate Nine";
+    break;
+  }
+
+  return answer;
+}
+
+chainToSwitch(7);
+
+//Returning Boolean Values from Functions
+//You may recall from Comparison with the Equality Operator that all comparison operators return a boolean true or false value.
+
+//Sometimes people use an if/else statement to do a comparison, like this:
+function isEqual(a, b) {
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+//But there's a better way to do this. Since === returns true or false, we can return the result of the comparison:
+function isEqual(a, b) {
+  return a === b;
+}
+
+/*Return Early Pattern for Functions
+When a return statement is reached, the execution of the current function stops and control returns to the calling location. */
+function abTest(a, b) {
+  if (a < 0 || b < 0) {
+  return undefined
+  }
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  abTest(2,2);
+
+  /* Counting Cards
+In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
+
+Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
+
+Count Change	Cards
++1	2, 3, 4, 5, 6
+0	7, 8, 9
+-1	10, 'J', 'Q', 'K', 'A'
+You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
+
+Example Outputs: -3 Hold or 5 Bet */
+let count = 0;
+
+function cc(card) {
+ switch(card) {
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+   count++;
+   break;
+  case 10:
+  case "J":
+  case "Q":
+  case "K":
+  case "A":
+    count--;
+    break;
+ }
+ var betOrHold = "Hold";
+ if (count > 0) {
+   betOrHold = "Bet";
+ }
+
+  return count + " " + betOrHold;
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+/* Build JavaScript Objects
+You may have heard the term object before.
+
+Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+Objects are useful for storing data in a structured way, and can represent real world objects, like a cat. */
+const myDog = {
+  // Only change code below this line
+  "name": "Don",
+  "legs": 5,
+  "tails": 2,
+  "friends": ["Water", "Other Dogs"]
+
+  // Only change code above this line
+};
+
+/* Accessing Object Properties with Dot Notation
+There are two ways to access the properties of an object: dot notation (.) and bracket notation ([]), similar to an array.
+
+Dot notation is what you use when you know the name of the property you're trying to access ahead of time.
+
+Here is a sample of using dot notation (.) to read an object's property: */
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+/* Accessing Object Properties with Bracket Notation
+The second way to access the properties of an object is bracket notation ([]). If the property of the object you are trying to access has a space in its name, you will need to use bracket notation.
+
+However, you can still use bracket notation on object properties without spaces.
+
+Here is a sample of using bracket notation to read an object's property: */
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+const entreeValue = testObj["an entree"];
+const drinkValue = testObj["the drink"];
+
+/* Accessing Object Properties with Variables
+Another use of bracket notation on objects is to access a property which is stored as the value of a variable. This can be very useful for iterating through an object's properties or when accessing a lookup table.
+
+Here is an example of using a variable to access a property: */
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+const playerNumber = 16; 
+const player = testObj[playerNumber];
+
+/* Updating Object Properties
+After you've created a JavaScript object, you can update its properties at any time just like you would update any other variable. You can use either dot or bracket notation to update. */
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.name = "Happy Coder"
+
+/* Add New Properties to a JavaScript Object
+You can add new properties to existing JavaScript objects the same way you would modify them. */
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.bark = "woof"
+
+/* Delete Properties from a JavaScript Object
+We can also delete properties from objects like this: */
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+delete myDog.tails;
+
+/* Using Objects for Lookups
+Objects can be thought of as a key/value storage, like a dictionary. If you have tabular data, you can use an object to lookup values rather than a switch statement or an if/else chain. This is most useful when you know that your input data is limited to a certain range. */
+function phoneticLookup(val) {
+  let result = "";
+  var lookup = {
+    "alpha" : "Adams",
+    "bravo": "Boston",
+    "charlie" : "Chicago",
+    "delta" : "Denver",
+    "echo" : "Easy",
+    "foxtrot" : "Frank",
+  }
+  result = lookup[val];
+  return result;
+}
+
+phoneticLookup("charlie");
